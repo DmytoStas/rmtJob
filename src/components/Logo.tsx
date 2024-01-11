f584d4aa6976f30feb91d8d29bc333e25adb6701
Dmytro Stas <dima7stas777@gmail.com>
2024-01-10 22:38:55 -0500

    logo href url fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@/components";
 
 export default function Logo() {
   return (
-    <a href="/" className="logo">
+    <a href="/rmtJob/" className="logo">
       <Icon id="logo" className="logo__icon" />
       rmt<i className="logo__accent">Job</i>
     </a>
