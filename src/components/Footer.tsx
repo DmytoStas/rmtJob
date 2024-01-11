e8320dc22d097581d4bb9a783927080e5dc42a9d
Dmytro Stas <dima7stas777@gmail.com>
2024-01-10 21:30:31 -0500

    Responsive design + footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,19 @@
 export default function Footer() {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by{" "}
+          <a href="https://dmytro-stas.vercel.app/" target="_blank">
+            DmytroStas
+          </a>
+          . Intended for portfolio.
+        </p>
+      </small>
+
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
 }
